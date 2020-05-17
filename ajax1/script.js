b9b1e2519c7ee828549ea0952bea90c0ee4e46b7
GodlyHamster <28106@ma-web.nl>
2020-05-17 15:06:40 +0200

    Completed quiz

--- ajax1/script.js
+++ ajax1/script.js
@@ -1,13 +1,21 @@
-let loadDocument = document.getElementById('loadDocument');
-loadDocument.addEventListener('click', ajax);
+let firstName = document.getElementById('firstName');
+let age = document.getElementById('age');
+let submitButton = document.getElementById('submitButton');
+let responseHere = document.getElementById('responseHere');
+
+submitButton.addEventListener('click', ajax);
 
 function ajax() {
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
-      document.getElementById('demo').innerHTML = this.responseText;
+      responseHere.innerHTML = this.responseText;
     }
   };
-  xmlhttp.open("GET", "newText.txt", true);
+  let httpString = "form_1.php?firstName=" +firstName.value+ "&age=" +age.value;
+
+  console.log(httpString);
+
+  xmlhttp.open("GET", httpString, true);
   xmlhttp.send();
 }
